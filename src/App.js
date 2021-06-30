a205d5d9e875b882526b5ba0104eaf879369e71e
Aleksandar Milosevic <alexmilosevic992@gmail.com>
2021-06-30 17:27:50 +0200

    header.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>React Router Tutorial</h1>
+      <Header />
     </div>
   );
 }
